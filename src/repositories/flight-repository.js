2eb278cc0d13af5015b706885931860aea5d2394
Yaafay <muhammad.yaffay@gmail.com>
2025-04-04 18:50:52 +0500

    Added booking with transaction logic

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 const CrudRepository = require("./crud-repository");
 const { Flight, Airplane, Airport, City, sequelize } = require("../models");
 const db = require("../models");
-const {addRowLockOnFlights} = require("./queries")
+const { addRowLockOnFlights } = require("./queries");
 
 class FlightRepository extends CrudRepository {
   constructor() {
@@ -57,14 +57,29 @@ class FlightRepository extends CrudRepository {
   }
 
   async updateRemainingSeats(flightId, seats, dec = true) {
-    await db.sequelize.query(addRowLockOnFlights(flightId))  //row level locking for handling concurrent updates (put row lock on any kind of update)
-    const flight = await Flight.findByPk(flightId);
-    if (+dec) {
-      await flight.decrement("totalSeats", { by: seats });
-    } else {
-      await flight.increment("totalSeats", { by: seats });
+    const transaction = await db.sequelize.transaction();
+    try {
+      await db.sequelize.query(addRowLockOnFlights(flightId)); //row level locking for handling concurrent updates (put row lock on any kind of update)
+      const flight = await Flight.findByPk(flightId);
+      if (+dec) {
+        await flight.decrement(
+          "totalSeats",
+          { by: seats },
+          { transaction: transaction }
+        );
+      } else {
+        await flight.increment(
+          "totalSeats",
+          { by: seats },
+          { transaction: transaction }
+        );
+      }
+      await transaction.commit();
+      return flight;
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
     }
-    return flight
   }
 }
 
